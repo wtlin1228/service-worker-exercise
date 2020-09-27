91d9fb5d72a76752dd7f71accb44115eb0b665c1
wtlin <wtlin1228@gmail.com>
2020-09-27 17:14:17 +0800

    detect offline

--- web/js/blog.js
+++ web/js/blog.js
@@ -2,7 +2,7 @@
   "use strict";
 
   var offlineIcon;
-
+  var isOnline = "onLine" in navigator ? navigator.onLine : true;
   var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || "");
 
   document.addEventListener("DOMContentLoaded", ready, false);
@@ -11,5 +11,19 @@
 
   function ready() {
     offlineIcon = document.getElementById("connectivity-status");
+
+    if (!isOnline) {
+      offlineIcon.classList.remove("hidden");
+    }
+
+    window.addEventListener("online", function online() {
+      offlineIcon.classList.add("hidden");
+      isOnline = true;
+    });
+
+    window.addEventListener("offline", function offline() {
+      offlineIcon.classList.remove("hidden");
+      isOnline = false;
+    });
   }
 })();
