89eb0aabadd700a2b37f86d0779ad52e99f5ec2b
wtlin <wtlin1228@gmail.com>
2020-10-02 02:44:03 +0800

    message hangling between sercive worker and client

--- web/js/sw.js
+++ web/js/sw.js
@@ -1,21 +1,44 @@
 "use strict";
 
-const version = 1;
+const version = 2;
+var isOnline = true;
+var isLoggedIn = false;
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
+self.addEventListener("message", onMessage);
 
 main().catch(console.error);
 
 // ****************************************************
 
 async function main() {
-  console.log(`Service Worker (${version}) is starting...`);
+  await sendMessage({ requestStatusUpdate: true });
 }
 
 async function onInstall(evt) {
   console.log(`Service Worker (${version}) is installed.`);
   self.skipWaiting();
+}
+
+async function sendMessage(msg) {
+  var allClients = await clients.matchAll({ includeUncontrolled: true });
+  return Promise.all(
+    allClients.map(function clientMsg(client) {
+      var chan = new MessageChannel();
+      chan.port1.onmessage = onMessage;
+      return client.postMessage(msg, [chan.port2]);
+    })
+  );
+}
+
+function onMessage({ data }) {
+  if (data.statusUpdate) {
+    ({ isOnline, isLoggedIn } = data.statusUpdate);
+    console.log(
+      `Service Worker (v${version}) status update, isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`
+    );
+  }
 }
 
 function onActivate(evt) {
