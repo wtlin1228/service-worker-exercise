4af94e83af9fecc2e38747ad79263c0d212d4ada
wtlin <wtlin1228@gmail.com>
2020-10-02 18:02:02 +0800

    add service worker cache

--- web/js/sw.js
+++ web/js/sw.js
@@ -1,8 +1,27 @@
 "use strict";
 
-const version = 2;
+const version = 4;
 var isOnline = true;
 var isLoggedIn = false;
+var cacheName = `ramblings-${version}`;
+
+var urlToCache = {
+  loggedOut: [
+    "/",
+    "/about",
+    "/contact",
+    "/404",
+    "/login",
+    "/offline",
+    "/css/style.css",
+    "/js/blog.js",
+    "/js/home.js",
+    "/js/login.js",
+    "/js/add-post.js",
+    "/images/logo.gif",
+    "/images/offline.png",
+  ],
+};
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
@@ -14,6 +33,7 @@ main().catch(console.error);
 
 async function main() {
   await sendMessage({ requestStatusUpdate: true });
+  await cacheLoggedOutFiles();
 }
 
 async function onInstall(evt) {
@@ -36,7 +56,7 @@ function onMessage({ data }) {
   if (data.statusUpdate) {
     ({ isOnline, isLoggedIn } = data.statusUpdate);
     console.log(
-      `Service Worker (v${version}) status update, isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`
+      `Service Worker (v${version}) status update... isOnline:${isOnline}, isLoggedIn:${isLoggedIn}`
     );
   }
 }
@@ -46,6 +66,51 @@ function onActivate(evt) {
 }
 
 async function handelActivation() {
+  await clearCaches();
+  await cacheLoggedOutFiles(/*forceReload=*/ true);
   await clients.claim();
   console.log(`Service Worker (${version}) is activated.`);
+}
+
+async function clearCaches() {
+  var cacheNames = await caches.keys();
+  var oldCacheNames = cacheNames.filter(function matchOldCache(cacheName) {
+    var [, cacheVersion] = cacheName.match(/^ramblings-(\d+)$/) || [];
+    cacheVersion = cacheVersion != null ? Number(cacheVersion) : cacheVersion;
+    return cacheVersion > 0 && cacheVersion !== version;
+  });
+  await Promise.all(
+    oldCacheNames.map(function deleteCache(cacheName) {
+      return caches.delete(cacheName);
+    })
+  );
+}
+
+async function cacheLoggedOutFiles(forceReload = false) {
+  var cache = await caches.open(cacheName);
+
+  return Promise.all(
+    urlToCache.loggedOut.map(async function cacheFile(url) {
+      try {
+        let res;
+
+        if (!forceReload) {
+          res = await cache.match(url);
+          if (res) {
+            return res;
+          }
+        }
+
+        let fetchOptions = {
+          method: "GET",
+          cache: "no-cache",
+          credentials: "omit",
+        };
+        res = await fetch(url, fetchOptions);
+        if (res.ok) {
+          await cache.put(url, res);
+        }
+      } catch (err) {}
+    })
+  );
 }
